c9b45148a73ac8902d25080f8a65d91e88a9bdcd
Mansi-sketch <129894321+Mansi-sketch@users.noreply.github.com>
2025-10-08 01:36:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 function insert_Row() {
-    //Write your code here
-  
-  
+	let table = document.getElementById('sampleTable');
+	let insertAtTop = table.insertRow(0);
+	insertAtTop.innerHTML +=  '<tr><td>New Cell1</td><td>New Cell2</td></tr>'
 }
